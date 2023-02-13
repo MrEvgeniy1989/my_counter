77fd7209a1f3c5ff6f63731790b03229892299c5
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2023-02-13 22:12:06 +0300

    сверстал и добавил немного логики

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,14 +1,43 @@
-import React from 'react';
-import styles from "./Counter.module.css"
+import React, {useState} from 'react';
+import styles from './Counter.module.css'
 
-type CounterPropsType = {
-
-}
+type CounterPropsType = {}
 
 export const Counter = (props: CounterPropsType) => {
+    const [value, setValue] = useState<number>(0)
+    const [minValue, setMinValue] = useState<number>(0)
+    const [maxValue, setMaxValue] = useState<number>(5)
+
+    const inc = () => {
+        if (value >= minValue && value < maxValue) {
+            setValue(value + 1)
+        }
+
+    }
+    const reset = () => {
+      setValue(minValue)
+    }
+
     return (
         <div className={styles.counter}>
-            <input type="text"/>
+            <div className={(value >= minValue && value < maxValue) ? styles.tablo : styles.redTablo}>
+                {value}
+            </div>
+            <div className={styles.buttons}>
+                <button
+                    className={styles.buttonInc}
+                    onClick={inc}
+                    disabled={value >= maxValue}
+                >Inc
+                </button>
+
+                <button
+                    className={styles.buttonReset}
+                    onClick={reset}
+                    disabled={value === minValue}
+                >Reset
+                </button>
+            </div>
         </div>
     )
 }
